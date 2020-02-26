67f0683b3c9206b7eefc51444e33cb54444dd2e5
maxim_ch <maxim_ch@webant.ru>
2020-02-26 10:37:53 +0300

    v1.1

--- GalleryCleanSwift/Main/MainInteractor.swift
+++ GalleryCleanSwift/Main/MainInteractor.swift
@@ -7,3 +7,68 @@
 //
 
 import Foundation
+import RxAlamofire
+import RxSwift
+
+protocol MainInteractorProtocol: class {
+    var origin: Origin! { get set}
+    func loadImages(currentPage: Int, currentState: Int)
+}
+
+class MainInteractor: MainInteractorProtocol {
+    var currentElement: Photo!
+    
+    
+    var isNewDataLoading: Bool! = false
+    let serverService: ServerServiceProtocol = ServerService()
+    var origin: Origin! 
+    private var currentArray: [Photo]! = []
+    weak var presenter: MainPresenterProtocol!
+
+    required init(presenter: MainPresenterProtocol) {
+        self.presenter = presenter
+    }
+    
+    func loadImages(currentPage: Int, currentState: Int) {
+        var photosURL = ""
+        if currentState == 0 {
+            photosURL = "http://gallery.dev.webant.ru/api/photos?new=true&page="
+        }
+        else {
+            photosURL = "http://gallery.dev.webant.ru/api/photos?popular=true&page="
+        }
+               let url = URL(string: photosURL + String(currentPage) + "&limit=15")!
+        self.presenter.isNewDataLoading = true
+               RxAlamofire.requestData(.get, url)
+                   .asSingle()
+                   .map { (response, data) -> Origin in
+                       //                   print("Response: \(response)")
+                                           print("Data: \(data), it is photos")
+                       
+                       return try! JSONDecoder().decode(Origin.self, from: data)
+               }
+               .observeOn(MainScheduler.instance)
+               .subscribe(onSuccess: { [weak self] (model) in
+                   guard let self = self else {
+                       return
+                   }
+                   self.origin = model
+                   let startIndex = self.presenter.currentArray.count
+                   let endIndex = self.presenter.currentArray.count + self.origin.data.count
+                   let sequince = (startIndex..<endIndex).map{IndexPath(item: $0, section: 0)}
+                   print(sequince)
+                   self.presenter.isNewDataLoading = false
+                   self.presenter.currentArray.append(contentsOf: self.origin.data)
+                self.presenter.origin = model
+                if currentPage == 1 {
+                    self.presenter.view.reloadData()
+                }
+                else {
+                    self.presenter.view.insertItems(sequince: sequince)
+                }
+               }) { (error) in
+                   //handle error
+                   print("Rx ERROR")
+               }
+       }
+}
---
+++ Pods/Headers/Public/RxNetworkApiClient/RxNetworkApiClient-umbrella.h
@@ -0,0 +1,1 @@
+../../../Target Support Files/RxNetworkApiClient/RxNetworkApiClient-umbrella.h
